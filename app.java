cbf88c8e5267255cfa8cd0d25f6abe2cbb59ab45
Emmadevops <echemebeng@yahoo.com>
2022-02-26 08:13:04 -0800

    my commit

---
+++ app.java
@@ -0,0 +1,7 @@
+echo "devops is good"
+echo "enter your name"
+read name
+echo "enter your pasword"
+read -s pasword
+echo "congratulation, your credentials have been validated"
+echo "welcome to rbc"
